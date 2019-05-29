c2e698a60cfd463deb4fdba417bfc5863053cb6e
MiroslavBurda <35538660+MiroslavBurda@users.noreply.github.com>
2019-05-29 12:11:46 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ int pTrig[8] = {13, 10, 11, 12, A0, A1, A2, A3};
 int pEcho[8] = {2, 3, 4, 5, 6, 7, 8, 9};
 long response[8]; 
 float distance[8];
-int min_first = 500; 
-int min_last = 500;
+int min_first = 32500; 
+u_int8 min_last = 254;
 
 void setup()  {
   Serial.begin(115200);
@@ -41,9 +41,18 @@ void loop()
     Serial.print("; ");
     delay(50); // without this, it will measure max. distance cca 130 cm
   }
+  if (min > 255) 
+      min = 255;
   min_last = min;
   Serial.println(min_last);
   delay(500);  // TOHLE V OSTRE VERZI PRYC ************************************
   Serial.print(millis()); // TOHLE TAKY 
 }
+
+https://uart.cz/271/arduino-a-preruseni/
+pin rising - nastavit v hlavním programu - nezapomenout ho zase sesadit 
+posílat chci pouze 1 bajt -> jak to udělat z typu int
+počkám na něj ve smyčce + ošetrit konec čekání a chybovou hlášku, když bude problém 
+
+
 
